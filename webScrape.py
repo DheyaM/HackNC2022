998b3f39c24ce50b08a42e63c4fb913699e87124
Dheya Madhani <dheyam@gmail.com>
2022-11-05 13:36:39 -0400

    scraped data for biden

--- webScrape.py
+++ webScrape.py
@@ -1,16 +1,28 @@
 from bs4 import BeautifulSoup
 
 import requests
-import json 
+import json
 
 # soup = BeautifulSoup(html_doc, 'html.parser')
 
 url ='https://www.politifact.com/truth-o-meter/promises/list/?promise_group=biden-promise-tracker&ruling=promise-kept'
 response = requests.get(url)
+
 print(response.status_code) # should be 200
-# convert json file
-data_json = response.json()
-print(type(data_json))
-for x in data_json:
-    print(x)
-print("\n")
+soup = BeautifulSoup(response.content, "html.parser")
+print(soup)
+# element that contains the promises made
+results = soup.find_all("div", class_="m-statement__quote")
+print(results)
+print(type(results))
+
+# data = json.loads(response.text)[0]
+
+data = []
+
+for result in results:
+    data.append(result.find('a'))
+
+print(data)
+print(type(data))
+print(data[1])
