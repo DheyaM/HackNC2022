ce72fe67f3b86aa393b8a829f33e315beb6c614d
Sneha Jaikumar <snehaj@email.unc.edu>
2022-11-05 14:12:59 -0400

    updated code

--- webScrape.py
+++ webScrape.py
@@ -1,16 +1,21 @@
+# run this in terminal before running the notebook:
+# pip install bs4
+# pip install requests
+
 from bs4 import BeautifulSoup
-
 import requests
-import json 
-
-# soup = BeautifulSoup(html_doc, 'html.parser')
 
 url ='https://www.politifact.com/truth-o-meter/promises/list/?promise_group=biden-promise-tracker&ruling=promise-kept'
 response = requests.get(url)
+
 print(response.status_code) # should be 200
-# convert json file
-data_json = response.json()
-print(type(data_json))
-for x in data_json:
-    print(x)
-print("\n")
+soup = BeautifulSoup(response.content, "html.parser")
+# element that contains the promises made
+results = soup.find_all("div", class_="m-statement__quote")
+
+data = []
+for result in results:
+    data.append(result.find('a'))
+
+print(data)
+print(type(data))
